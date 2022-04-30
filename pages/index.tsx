d2bbf15d39ce00197ad420448036d3e3bf7acf2c
Klish <cory.klish@cerner.com>
2022-04-30 15:51:08 -0400

    Add link to /view-coin

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,30 @@ import type { NextPage } from 'next'
 import MintModal from '../components/MintModal'
 import { useState } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   const [opened, setOpened] = useState(false)
   return (
-    <div className="flex justify-center items-center h-screen">
-      <MintModal {...{ opened, setOpened }} />
+    <>
+      <nav className="flex flex-row p-5">
+        <Link href={'/view-coin'}>
+          <a className="text-xl">View Coin</a>
+        </Link>
+      </nav>
+      <div className="flex justify-center items-center h-screen">
+        <MintModal {...{ opened, setOpened }} />
 
-      <button onClick={() => setOpened(true)}>
-        <Image
-          alt="Coin Slot Machine"
-          src={'/images/coin-slot.svg'}
-          width={500}
-          height={500}
-        ></Image>
-      </button>
-    </div>
+        <button onClick={() => setOpened(true)}>
+          <Image
+            alt="Coin Slot Machine"
+            src={'/images/coin-slot.svg'}
+            width={500}
+            height={500}
+          ></Image>
+        </button>
+      </div>
+    </>
   )
 }
 
